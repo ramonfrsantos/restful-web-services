d74cf817e8be85e1ce73b10f8a2f5d54d848019f
Ramon Felipe Ribeiro Santos <ramonfrsantos@outlook.com>
2022-08-18 14:03:14 -0300

    dynamic filtering with some bean

--- src/main/java/br/com/practice/rest/webservices/restfulwebservices/filter/FilteringController.java
+++ src/main/java/br/com/practice/rest/webservices/restfulwebservices/filter/FilteringController.java
@@ -1,13 +1,30 @@
 package br.com.practice.rest.webservices.restfulwebservices.filter;
 
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 
 @RestController
 public class FilteringController {
   
   @GetMapping("/filtering")
-  public SomeBean retrieveSomeBean(){
-    return new SomeBean("value1", "value2", "value3");
+  public MappingJacksonValue retrieveSomeBean(){
+    SomeBean someBean = new SomeBean("value1", "value2", "value3");
+
+    String[] values ={"field1", "field3"};
+
+    SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept(values);
+
+    FilterProvider filters = new SimpleFilterProvider().addFilter("SomeBeanFilter", filter);
+
+    MappingJacksonValue mapping = new MappingJacksonValue(someBean);
+
+    mapping.setFilters(filters);
+
+    return mapping;
   }
 }
--- src/main/java/br/com/practice/rest/webservices/restfulwebservices/filter/SomeBean.java
+++ src/main/java/br/com/practice/rest/webservices/restfulwebservices/filter/SomeBean.java
@@ -1,12 +1,11 @@
 package br.com.practice.rest.webservices.restfulwebservices.filter;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonFilter;
 
+@JsonFilter("SomeBeanFilter")
 public class SomeBean {
   private String field1;
   private String field2;
-
-  @JsonIgnore
   private String field3;
 
   public SomeBean(String field1, String field2, String field3) {
